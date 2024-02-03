c23798fb0bcc6567ebc2da2945dfbaad3e4d7bee
AfrinaKhatun <bit0411@iit.du.ac.bd>
2024-02-03 18:22:11 +0600

    Trying graphql

--- main.py
+++ main.py
@@ -15,33 +15,8 @@ if __name__ == '__main__':
 
 import requests
 import json
+import statistics
 
-baseUrl = 'https://api.github.com'
-owner='Kaggle'
-accessToken='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'
-#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
-headers = {'Accept':'application/vnd.github+json','X-GitHub-Api-Version':'2022-11-28'}
-parameters = {'Authorization': 'Bearer '+accessToken}
-
-#response = requests.get(baseUrl+'/orgs/'+owner+'/repos', params=parameters, headers=headers)
-#print(response.json())
-#for repo in response.json():
-    #print(repo['name'])
-    #repoUrl=baseUrl+'/repos/'+owner+'/'+repo['name']
-    #r=requests.get(repoUrl,params=parameters, headers=headers)
-    #print(r)
-    #starCount=r.json()['stargazers_count']
-    #print(starCount)
-    #forkCount=r.json()['forks_count']
-    #print(starCount)
-    #branchUrl=baseUrl+'/repos/'+owner+'/'+repo['name']+'/branches'
-    #r = requests.get(branchUrl, params=parameters, headers=headers)
-    #branchCount=len(r.json())
-    #print(branchCount)
-    #branchNames=[]
-    #for b in r.json():
-        #branchNames.append(b['name'])
-    #print(branchNames)
 
 url = "https://api.github.com/graphql"
 organizationName='Kaggle'
@@ -57,18 +32,19 @@ query{
   }
 }
 """
-head = {'Authorization': 'Bearer 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'}
+head = {'Authorization': 'Bearer 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'}
 response = requests.post(url=url, json={"query": getAllRepos},headers=head)
 print("response status code: ", response.status_code)
 if response.status_code == 200:
     print("response : ", response.content)
 numberOfRepos=response.json()['data']['organization']['repositories']['totalCount']
 nameOfRepos=response.json()['data']['organization']['repositories']['nodes']
-query="""
+
+getAllInfoQuery="""
 query{
 
 """
-x=0
+counter=0
 for repo in nameOfRepos:
     print(repo['name'])
     tempQuery="""
@@ -106,95 +82,126 @@ for repo in nameOfRepos:
     }
   }
     """
-    query=query+'repo'+str(x)+tempQuery+""",
+    getAllInfoQuery=getAllInfoQuery+'repo'+str(counter)+tempQuery+""",
     """
-    x=x+1
-query=query+"""
+    counter=counter+1
+getAllInfoQuery=getAllInfoQuery+"""
 }"""
 
-counter=0
-
-response = requests.post(url=url, json={"query": query},headers=head)
+response = requests.post(url=url, json={"query": getAllInfoQuery},headers=head)
 print("response status code: ", response.status_code)
+
+# ----- Another query required for TAGS only ----
+counter=0
+getTagsQuery="""
+query{
+
+"""
+for repo in nameOfRepos:
+    tempQuery="""
+    : repository(owner:\""""+organizationName+"""\", name:\""""+repo['name']+"""\") {
+        refs(refPrefix: "refs/tags/", first: 100){
+          totalCount
+        }
+    }
+    """
+    getTagsQuery=getTagsQuery+'repo'+str(counter)+tempQuery+""",
+    """
+    counter=counter+1
+getTagsQuery=getTagsQuery+"""
+}"""
+
+getTagsResponse = requests.post(url=url, json={"query": getTagsQuery},headers=head)
+print("response status code: ", getTagsResponse.status_code)
+
+
+counter=0
+organizationData={}
+repoData=[]
+starList=[]
+forkList=[]
+closedIssuesList=[]
+releasesList=[]
+commitsList=[]
+tagsList=[]
+branchList=[]
+languageList=[]
+
 if response.status_code == 200:
-    organizationData={}
-    repoData=[]
     while counter<numberOfRepos:
         repo=response.json()['data']['repo'+str(counter)]
         tempData={}
         tempData['name']=repo['name']
         tempData['stars']=repo['stargazerCount']
+        starList.append(repo['stargazerCount'])
         tempData['forks']=repo['forkCount']
+        forkList.append(repo['forkCount'])
+        tempData['closedIssues']=repo['issues']['totalCount']
+        closedIssuesList.append(repo['issues']['totalCount'])
+        tempData['releases']=repo['releases']['totalCount']
+        releasesList.append(repo['releases']['totalCount'])
+
+        languageStatistics={}
+        languageStatistics['numberOfLanguage']=repo['languages']['totalCount']
+        languageList.append(repo['languages']['totalCount'])
+        languageStatistics['totalSizeInBytesofFilesWrittenInLanguage']=repo['languages']['totalSize']
+        listOfLanguages=[]
+        for l in range(repo['languages']['totalCount']):
+            language = {}
+            language['name']=repo['languages']['nodes'][l]['name']
+            language['sizeInBytesofFilesWrittenInLanguage'] = repo['languages']['edges'][l]['size']
+            listOfLanguages.append(language)
+        languageStatistics['listOfLanguage']=listOfLanguages
+        tempData['languageStatistics']=languageStatistics
+
+        branchStatistics={}
+        listOfBranches=[]
+        totalCommits=0
+        branchList.append(len(repo['refs']['nodes']))
+        for b in range(len(repo['refs']['nodes'])):
+            branch={}
+            branch['name']=repo['refs']['nodes'][b]['name']
+            branch['branchCommits'] = repo['refs']['nodes'][b]['target']['history']['totalCount']
+            totalCommits=totalCommits + repo['refs']['nodes'][b]['target']['history']['totalCount']
+            listOfBranches.append(branch)
+        branchStatistics['numberOfBranch']=len(repo['refs']['nodes'])
+        branchStatistics['listOfBranches']=listOfBranches
+        tempData['branchStatistics']=branchStatistics
+        tempData['commits']=totalCommits
+        commitsList.append(totalCommits)
+
+        tempData['tags']=getTagsResponse.json()['data']['repo'+str(counter)]['refs']['totalCount']
+        tagsList.append(tempData['tags'])
+
         repoData.append(tempData)
         organizationData[organizationName]=repoData
         counter=counter+1
-    json_data = json.dumps(organizationData, indent=2)
-    file_path = "Data/AllRepositoryInformation.json"
-    with open(file_path, 'w') as json_file:
-        json_file.write(json_data)
 
 
 
 
+overAllStatistics={}
+overAllStatistics['totalStars']=sum(starList)
+overAllStatistics['medianStars']=statistics.median(starList)
+overAllStatistics['totalForks']=sum(forkList)
+overAllStatistics['medianForks']=statistics.median(forkList)
+overAllStatistics['totalClosedIssues']=sum(closedIssuesList)
+overAllStatistics['medianClosedIssues']=statistics.median(closedIssuesList)
+overAllStatistics['totalRelease']=sum(releasesList)
+overAllStatistics['medianRelease']=statistics.median(releasesList)
+overAllStatistics['totalCommits']=sum(commitsList)
+overAllStatistics['medianCommits']=statistics.median(commitsList)
+overAllStatistics['totalTags']=sum(tagsList)
+overAllStatistics['medianTags']=statistics.median(tagsList)
+overAllStatistics['totalBranches']=sum(branchList)
+overAllStatistics['medianBranches']=statistics.median(branchList)
+overAllStatistics['totalLangugaes']=sum(languageList)
+overAllStatistics['medianLanguages']=statistics.median(languageList)
 
-body = """
-query {
-  repoA: repository(owner:"Kaggle", name:\""""+'gyfy'+"""\") {
-    name
-    forkCount
-    stargazerCount
-    releases(first:0){
-      totalCount
-    }
-    issues(states:CLOSED) {
-      totalCount
-    }
-    languages(first:10) {
-      totalCount
-      totalSize
-      edges{
-        size
-      }
-      nodes{
-        name 
-      }
-    }
-	refs(refPrefix: "refs/heads/", first: 100){
-        nodes{
-          name
-          target {
-          ... on Commit {
-            id
-            history(first: 0) {
-              totalCount
-            }
-          }
-        }
-        }
-    }
-  },
-  repoB: repository(owner:"Kaggle", name:"docker-python") {
-    name
-    forkCount
-    releases(first:0){
-      totalCount
-    }
-     refs(refPrefix: "refs/heads/", first: 100){
-        nodes{
-          name
-          target {
-          ... on Commit {
-            id
-            history(first: 0) {
-              totalCount
-            }
-          }
-        }
-        }
-    }
-    issues(states:CLOSED) {
-      totalCount
-    }
-  }
-}
-"""
+organizationData['overAllStatistics']=overAllStatistics
+
+json_data = json.dumps(organizationData, indent=2)
+file_path = "Data/AllRepositoryInformation.json"
+with open(file_path, 'w') as json_file:
+    json_file.write(json_data)
+
